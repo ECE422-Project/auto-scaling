313424600aecc079b1b45a258a1a88b360b42959
Dekr0 <dekr0.dk@gmail.com>
2023-04-14 07:23:09 +0000

    Separate Graph

--- graph/main.js
+++ graph/main.js
@@ -1,37 +1,12 @@
-async function updateNumReplications(chart) {
-    const response = await fetch('http://10.2.6.145:3000/status');
-    const json = await response.json();
-    const now = Date.now();
-    for (let i = 0; i < json.length; i++) {
-        chart.data.datasets[i].data.push({
-            x: now,
-            y: json[i]
-        });
-    }
-}
-
-
-const config = {
+const replicasConfig = {
     type: 'line',
     data: {
         datasets: [
             {
-                label: 'Average Response Time',
-                backgroundColor: 'rgba(10, 113, 226, 0.8)',
-                borderColor: 'rgb(10, 113, 226)',
-                data: [],
-            },
-            {
                 label: 'Number of Replications',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
                 borderColor: 'rgb(255, 99, 132)',
-                data: [], 
-            },
-            {
-                label: 'Workload',
-                backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                borderColor: 'rgba(0, 0, 0)',
-                data: [],
+                data: []
             }
         ]
     },
@@ -40,7 +15,85 @@ const config = {
             x: {
                 type: 'realtime',
                 realtime: {
-                    onRefresh: updateNumReplications
+                    onRefresh: async (chart) => {
+                        const r = await fetch('http://10.2.6.145:3000/replicas');
+                        const json = await r.json();
+                        const now = Date.now();
+                        chart.data.datasets[0].data.push(
+                            {
+                                x: now,
+                                y: json
+                            }
+                        );
+                    }
+                }
+            }
+        }
+    }
+};
+
+const timeConfig = {
+    type: 'line',
+    data: {
+        datasets: [
+            {
+                label: 'Average Response Time',
+                backgroundColor: 'rgba(10, 113, 226, 0.8)',
+                borderColor: 'rgb(10, 113, 226)',
+                data: []
+            }
+        ]
+    },
+    options: {
+        scales: {
+            x: {
+                type: 'realtime',
+                realtime: {
+                    onRefresh: async (chart) => {
+                        const r = await fetch('http://10.2.6.145:3000/avg_response_time');
+                        const json = await r.json();
+                        const now = Date.now();
+                        chart.data.datasets[0].data.push(
+                            {
+                                x: now,
+                                y: json
+                            }
+                        );
+                    }
+                }
+            }
+        }
+    }
+};
+
+const workloadConfig = {
+    type: 'line',
+    data: {
+        datasets: [
+            {
+                label: 'Workload',
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                borderColor: 'rgba(0, 0, 0)',
+                data: []
+            }
+        ]
+    },
+    options: {
+        scales: {
+            x: {
+                type: 'realtime',
+                realtime: {
+                    onRefresh: async (chart) => {
+                        const r = await fetch('http://10.2.6.145:3000/workload');
+                        const json = await r.json();
+                        const now = Date.now();
+                        chart.data.datasets[0].data.push(
+                            {
+                                x: now,
+                                y: json
+                            }
+                        );
+                    }
                 }
             }
         }
@@ -48,4 +101,6 @@ const config = {
 };
 
 
-const graph = new Chart(document.getElementById('graph'), config);
+new Chart(document.getElementById('replicas'), replicasConfig);
+new Chart(document.getElementById('time'), timeConfig);
+new Chart(document.getElementById('workload'), workloadConfig);
