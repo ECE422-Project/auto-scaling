576822d683ca29df087734b1f99c1e27e2b23f79
Dekr0 <dekr0.dk@gmail.com>
2023-04-14 06:06:42 +0000

    Update GET Rout

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,36 @@
-'user strict'
+'use strict'
+
 
 const fastify = require('fastify')();
 
-fastify.register(require('@fastify/redis'), {
-    host: '10.2.6.145',
-    port: 6379,
-    family: 4
-});
 
-fastify.register(require('@fastify/cors'), {
-    origin: "*",
-    methods: ["GET"],
-});
+fastify
+    .register(require('@fastify/redis'), { 
+        host: '10.2.6.145', 
+        port: 6379, // Redis port
+        family: 4   // 4 (IPv4) or 6 (IPv6)
+    })
+    .register(require('@fastify/cors'), {
+        origin: [ 'http://10.2.6.145:5173' ],
+        method: [ 'GET' ],
+    });
 
 
-fastify.get('/query', function (req, reply) {
+fastify.get('/status', function (req, reply) {
     const { redis } = fastify;
 
-    redis.get(req.query.key, (err, val) => {
-        console.log(val);
-
-        reply.send(err || val);
-    });
+    redis.mget(
+        'avg_response_time', 'num_replications', 'num_visitors', 
+        (err, vals) => {
+            if (err) {
+                reply.send(err);
+            } else {
+                const data = [];
+                vals.forEach(val => data.push(val || 0));
+                reply.send(data);
+            }
+        }
+    );
 });
 
 
